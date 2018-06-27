a8c0ffd8c7b7cf3fe2450ac1c4deb2d29b4420b5
Jared <jaredgglass@gmail.com>
2018-06-27 13:56:35 +0100

    MIGRATED

--- GameWork.Unity.Engine.States/ChangeSceneTickStateTransition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameWork.Unity.Engine.States
-{
-	public abstract class ChangeSceneTickStateTransition : ChangeSceneEventStateTransition
-	{
-		protected ChangeSceneTickStateTransition(string toStateName, string toSceneName) : base(toStateName, toSceneName)
-		{
-		}
-	}
-}
